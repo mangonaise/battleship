91cc5d875bc2c307d255d6c41aefb8b57ae81a9f
= <liam2robertson@gmail.com>
2021-01-24 17:31:37 +0000

    Added hit, sink to ship object

--- src/logic/ship.ts
+++ src/logic/ship.ts
@@ -1,6 +1,19 @@
 const createShip = (initSize: number) => {
+  let hits = initializeHits();
+
+  function initializeHits() {
+    return Array.from({ length: initSize }).map(x => false);
+  }
+
+  function hitShip(index: number) {
+    hits[index] = true;
+  }
+
   return {
-    size: initSize
+    get size() { return initSize; },
+    get hits() { return hits; },
+    get hitShip() { return hitShip; },
+    get isSunk() { return hits.every(hit => hit === true); }
   }
 }
 
--- src/tests/ship.test.ts
+++ src/tests/ship.test.ts
@@ -1,9 +1,28 @@
 import createShip from '../logic/ship';
 
 describe('Ship', () => {
-  it('Initializes correctly', () => {
+  it('initializes correctly', () => {
     const ship = createShip(4);
     expect(ship.size).toBe(4);
+    expect(ship.hits).toEqual([false, false, false, false]);
   });
+
+  it('can be hit', () => {
+    const ship = createShip(3);
+    ship.hitShip(1);
+    expect(ship.hits).toEqual([false, true, false]);
+    ship.hitShip(2);
+    expect(ship.hits).toEqual([false, true, true]);
+    ship.hitShip(0);
+    expect(ship.hits).toEqual([true, true, true]);
+  })
+
+  it('becomes sunk if all units are hit', () => {
+    const ship = createShip(2);
+    expect(ship.isSunk).toEqual(false);
+    ship.hitShip(0);
+    ship.hitShip(1);
+    expect(ship.isSunk).toEqual(true);
+  })
 })
 

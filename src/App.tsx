c837f95f26e0ceee976091bfe10ec1a2e8b1d19c
= <liam2robertson@gmail.com>
2021-01-25 20:20:21 +0000

    Add basic board UI & interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import React from 'react';
+import GameBoardDisplay from './components/GameBoardDisplay';
+import Player from './logic/player';
+import './styles/App.css';
+
+const human = new Player('human');
+human.placeRandomShips();
+const cpu = new Player('cpu');
+
+const App = () => {
   return (
-    <div>
+    <div id="board-container">
+      <GameBoardDisplay player={human}/>
+      <GameBoardDisplay player={cpu} />
     </div>
   );
 }
+
 
 export default App;
